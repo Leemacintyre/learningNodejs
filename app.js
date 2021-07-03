cf8c7328c092102cfc5b2ccca62a5ea2deda278f
Leemacintyre <64503108+Leemacintyre@users.noreply.github.com>
2021-07-03 06:28:20 +0100

    connected sql database

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const path = require("path");
 const express = require("express");
-
-const errorController = require("./controllers/error");
+const db = require("./util/database");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+
+const errorController = require("./controllers/error");
 
 const app = express();
 
